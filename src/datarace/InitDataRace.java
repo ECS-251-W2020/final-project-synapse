23600ca69c50f5049b7a70e5c8deaa1edffecb40
nikhilwadhwa84 <44419564+nikhilwadhwa84@users.noreply.github.com>
2020-02-09 14:11:44 -0800

    Init DataRace Commit
    Start writing DataRace code

---
+++ src/datarace/InitDataRace.java
@@ -0,0 +1,12 @@
+package datarace;
+
+import java.util.ArrayList;
+
+public class InitDataRace {
+	
+
+	public static void main(String[] args) {
+		ArrayList<String> testList1 = new ArrayList<String>();
+		testList1.add("string1");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,3 @@
+module tsvd {
+	exports datarace;
+}
