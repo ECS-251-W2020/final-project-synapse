100ef7c1a3438d4ee5338a20a309dd76ae138203
prash <dprashureddy007@gmail.com>
2020-02-26 09:48:29 -0800

    add AllMethod.java

--- java-instrumentation-sample/src/main/java/com/company/MyFirstAgent.java
+++ java-instrumentation-sample/src/main/java/com/company/MyFirstAgent.java
@@ -1,6 +1,7 @@
 package com.company;
 import java.lang.instrument.Instrumentation;
 
+import net.bytebuddy.asm.Advice;
 import net.bytebuddy.matcher.ElementMatchers;
 import net.bytebuddy.agent.builder.AgentBuilder;
 
@@ -10,12 +11,21 @@ public class MyFirstAgent {
 
         System.out.println("Start!");
 //        inst.addTransformer(new MonitorTransformer());
+//        new AgentBuilder.Default()
+//                .type(ElementMatchers.any())
+//                .transform(new ByteBuddyTransformer())
+//                .with(AgentBuilder.Listener.StreamWriting.toSystemOut())
+//                .with(AgentBuilder.TypeStrategy.Default.REDEFINE)
+//                .installOn(inst);
+
         new AgentBuilder.Default()
-                .type(ElementMatchers.any())
-                .transform(new ByteBuddyTransformer())
-                .with(AgentBuilder.Listener.StreamWriting.toSystemOut())
-                .with(AgentBuilder.TypeStrategy.Default.REDEFINE)
-                .installOn(inst);
+                .with(new AgentBuilder.InitializationStrategy.SelfInjection.Eager())
+                .type((ElementMatchers.any()))
+                .transform((builder, typeDescription, classLoader, module) -> builder.method(ElementMatchers.any())
+                .intercept(Advice.to(AllMethod.class))
+        ).installOn(inst);
+
     }
+
 
 }
