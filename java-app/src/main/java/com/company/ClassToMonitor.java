6a923602fd7442636e5a0b665a2e318b55a412bc
prash <dprashureddy007@gmail.com>
2020-02-26 10:46:06 -0800

    clean old projects

--- java-app/src/main/java/com/company/ClassToMonitor.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class ClassToMonitor {
-    public void foo() {
-        int b = 1, c = 2;
-        int a[] = {b, c};
-        System.out.print(a.toString());
-    }
-}
