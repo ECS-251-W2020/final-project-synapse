af97fb707b17172697e561fccda0cd41adb0e40d
prash <dprashureddy007@gmail.com>
2020-02-29 20:16:12 -0800

    instrument arraylist.add()

--- java-profiler-app/src/com/company/ClassToMonitor.java
+++ java-profiler-app/src/com/company/ClassToMonitor.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ClassToMonitor {
@@ -7,5 +8,23 @@ public class ClassToMonitor {
         int b = 1, c = 2;
         int a[] = {b, c};
         System.out.print(Arrays.toString(a));
+    }
+    public void bar() {
+        // create an empty array list with an initial capacity
+        ArrayList<Integer> arrlist = new ArrayList<Integer>(5);
+
+        // use add() method to add elements in the list
+        arrlist.add(15);
+        arrlist.add(22);
+        arrlist.add(30);
+        arrlist.add(40);
+
+        // adding element 25 at third position
+        arrlist.add(2,25);
+
+        // let us print all the elements available in list
+        for (Integer number : arrlist) {
+            System.out.println("Number = " + number);
+        }
     }
 }
--- java-profiler-app/src/com/company/Main.java
+++ java-profiler-app/src/com/company/Main.java
@@ -9,8 +9,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("before loop: ");
         ClassToMonitor classToMonitor = new ClassToMonitor();
-        for (int i = 0; i < 10; i++) {
-            classToMonitor.foo();
+        for (int i = 0; i < 1; i++) {
+//            classToMonitor.foo();
+            classToMonitor.bar();
         }
         System.out.println("\nafter loop: " + Monitor.counter);
     }
